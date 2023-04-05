3b9761b7a4166af1d1e624a24d57a6b29e27546e
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-04-05 17:18:32 -0500

    completed 1-5

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
-
-// Exercise 3
+//exercise1&2
+const firstName = "Drake"; 
+let lastName = "Lopez";
+var age = 28;
+//exercise3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//exercise4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//exercise5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
